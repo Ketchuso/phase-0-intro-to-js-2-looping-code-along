74d495f01f23f2334007f504bb7672395a15a83d
Jay Davis <jdlikesbsbl@gmail.com>
2025-01-08 09:40:17 -0600

    Complete lab :)

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+const thanks = [];
+
+function writeCards(nameArray, message){
+    
+    for(let i = 0; i < nameArray.length; i++){
+        const messages = `Thank you, ${nameArray[i]}, for the wonderful ${message} gift!`;
+        thanks.push(messages);
+    } 
+    return thanks;
+}
+
+writeCards(nameArray, message);
+
+function countDown(num){
+    while(num >= 0){
+        console.log(num);
+        num--;
+    }
+}
